b1fa368ed47905cad08981e529fb69c0a121c1a6
Shubham Mishra <shubhammishra1442@gmail.com>
2023-12-02 01:04:46 -0800

    i created the shell script

---
+++ init_setup.sh
@@ -0,0 +1,8 @@
+echo [$(date)] : "START"
+echo [$(date)] : "creating environment with python 3.8 version"
+conda create --prefix ./env python=3.8 -y
+echo [$(date)] : "activating the environment"
+conda activate ./env
+echo [$(date)] : "installing the dev requirements"
+pip install -r requirements.txt
+echo [$(date)] : "END"
